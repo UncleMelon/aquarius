07849fb4b40b6f67e1431ecaea9f72a9e4f34998
Matthew Wu <yangwu1@paypal.com>
2022-07-11 12:46:31 +0800

    remove concurrency

--- concurrency/src/main/scala/basic/LazyValsObject.scala
+++
@@ -1,8 +0,0 @@
-package basic
-
-object LazyValsObject extends App {
-  object Lazy { log("Running Lazy constructor.") }
-  log("Main thread is about to reference Lazy.")
-  Lazy
-  log("Main thread completed.")
-}
